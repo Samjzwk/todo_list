0a5a5ac00785f7c6f970da6e1af4d3aa652aeff2
Samjzwk <jozwiaksamuel@gmail.com>
2020-09-16 17:26:13 +0200

    Refacto functinnal to classes

--- src/components/TaskList/Tasklist.js
+++ src/components/TaskList/Tasklist.js
@@ -1,65 +1,74 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import Task from '../Task/Task';
 import './TaskList.scss';
 import Modal from 'react-modal';
 
-const Tasklist = () => {
-    const [todos, setTodo] = useState([{ text: "Faire la léssive", status: "done" }, { text: "Faire le ménage", status: "todo" }, { text: "Préparer la formation", status: "todo" }])
-    const [newTask, setNewTask] = useState("");
-    const [modalIsOpen, setIsOpen] = React.useState(false);
 
-    const openModal = () => setIsOpen(true);
-    const closeModal = () => setIsOpen(false);
-
-    const deleteFunction = (index) => {
-        const newTodoList = [...todos];
-        newTodoList.splice(index, 1);
-        setTodo(newTodoList);
-    }
-
-    const changeStatus = (index) => {
-        const newTodoList = [...todos];
-        newTodoList[index].status = newTodoList[index].status === 'todo' ? 'done' : 'todo';
-        setTodo(newTodoList);
-    }
-
-    const addTask = (event) => {
-        event.preventDefault();
-        if(newTask !== ""){
-            setTodo([...todos,{text:newTask, status: "todo"}])
+class Tasklist extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            todos : [{ text: "Faire la léssive", status: "done" }, { text: "Faire le ménage", status: "todo" }, { text: "Préparer la formation", status: "todo" }],
+            newTask: "",
+            modalIsOpen: false
         }
-        setNewTask("");
-        closeModal();
     }
 
-    const tasks = todos.map((task, index) =>
+    openModal = () => this.setState({modalIsOpen: true});
+    closeModal = () => this.setState({modalIsOpen: false});
+
+    deleteFunction = (index) => {
+        const newTodoList = [...this.state.todos];
+        newTodoList.splice(index, 1);
+        this.setState({todos:newTodoList});
+    }
+
+    changeStatus = (index) =>{
+        const newTodoList = [...this.state.todos];
+        newTodoList[index].status = newTodoList[index].status === 'todo' ? 'done' : 'todo';
+        this.setState({todos:newTodoList});
+    }
+
+    addTask = (e) =>{
+        e.preventDefault();
+        if(this.state.newTask !== ""){
+            this.setState({todos:[...this.state.todos,{text:this.state.newTask, status: "todo"}]})
+        }
+        this.setState({newTask: ""});
+        this.closeModal();
+    }
+
+
+    render() {
+        const tasks = this.state.todos.map((task, index) =>
         <li className="todo__task" key={index}>
-            <Task text={task.text} status={task.status} index={index} changeStatus={changeStatus} deleteFunction={deleteFunction} />
+            <Task text={task.text} status={task.status} index={index} changeStatus={this.changeStatus} deleteFunction={this.deleteFunction} />
         </li>)
 
-    return (
-        <div className="todo__container" id="todo">
+        return (
+            <div className="todo__container" id="todo">
             <div className="todo__header">
                 <span>Que dois-je faire ?</span>
             </div>
             <ul className="todo__list">
                 {tasks}
             </ul>
-            <button className="todo__btn" onClick={openModal}>+ Nouvelle tache</button>
+            <button className="todo__btn" onClick={()=> this.openModal()}>+ Nouvelle tache</button>
             <Modal
-                isOpen={modalIsOpen}
-                onRequestClose={closeModal}
+                isOpen={this.state.modalIsOpen}
+                onRequestClose={() => this.closeModal()}
                 contentLabel="Example Modal"
                 className="Modal"
                 overlayClassName="Overlay"
             >
                 <span>Quel est la nouvelle tâche ?</span>
-                <input type="text" value={newTask} onChange={e=>setNewTask(e.target.value)} placeholder="tâches" />
-                <button type="submit" className="submit" onClick={(e)=>addTask(e)}>Ajouter</button>
-                <button onClick={closeModal} className="close">X</button>
+                <input type="text" value={this.state.newTask} onChange={e=>this.setState({newTask: e.target.value})} placeholder="tâches" />
+                <button type="submit" className="submit" onClick={(e)=>this.addTask(e)}>Ajouter</button>
+                <button onClick={() => this.closeModal()} className="close">X</button>
             </Modal>
         </div>
-    )
+        );
+    }
 }
 
 export default Tasklist;
